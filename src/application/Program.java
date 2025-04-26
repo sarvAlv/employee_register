23fe774780a6b8c606b0724a059f1c32263f1372
sarvAlv <sarviomesquita@gmail.com>
2025-04-26 10:24:43 -0300

    update

--- src/application/Program.java
+++ src/application/Program.java
@@ -12,26 +12,71 @@ public class Program {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
+        List<Employee> list = new ArrayList<>();
+
         System.out.print("How many enployees will be registered? ");
         int n = sc.nextInt();
 
-        List<Employee> employeesList = new ArrayList<>();
-
-        for (int i=1; i<=n; i++){
-            System.out.println("Employee #"+i);
+        for (int i = 0; i < n; i++) {
+            System.out.println();
+            System.out.println("Employee #" + (i + 1) + ":");
             System.out.print("ID: ");
             Integer id = sc.nextInt();
-            System.out.print("Name: ");
+            while (hasId(list,id)){
+                System.out.println("Id already taken! try again!");
+                id = sc.nextInt();
+            }
+
             sc.nextLine();
+            System.out.print("Name: ");
             String name = sc.nextLine();
             System.out.print("Salary: ");
             Double salary = sc.nextDouble();
-            employeesList.add(new Employee(id,name,salary));
+
+            Employee employee = new Employee(id, name, salary);
+
+            list.add(employee);
         }
-        for (Employee x : employeesList){
+        System.out.println();
+        System.out.print("Enter the employee id that will have salary increase: ");
+        int id = sc.nextInt();
+
+        Employee emp = list.stream().filter(x -> x.getId() == id).findFirst().orElse(null);
+
+        // Integer pos = position(list, id);
+
+        if (emp != null){
+            System.out.print("Enter the percentage: ");
+            double percentage = sc.nextDouble();
+            emp.increaseSalary(percentage);
+        }
+        else {
+            System.out.println("This id does not exist!");
+        }
+
+
+        System.out.println();
+        System.out.println("List of employees: ");
+        for (Employee x : list) {
             System.out.println(x);
         }
 
         sc.close();
     }
+
+    public static int position(List<Employee> list, int id) {
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).getId() == id) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static boolean hasId(List<Employee> list, int id){
+        Employee emp = list.stream().filter(x -> x.getId()== id).findFirst().orElse(null);
+        return emp != null;
+
+    }
+
 }
